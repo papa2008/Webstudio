db796e147fb6e145b89857b13fd899c3cbc2f73c
papa2008 <112895777+papa2008@users.noreply.github.com>
2023-08-10 18:10:41 +0200

    2

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector(".modal"),
+      backdrop: document.querySelector(".backdrop"),
+    };
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.backdrop.addEventListener("click", function(event) {
+      if (event.target === refs.backdrop) {
+        toggleModal();
+      }
+    });
+    function toggleModal() {
+      refs.modal.classList.toggle("is-visible");
+      refs.backdrop.classList.toggle("is-visible");
+    }
+  })();
